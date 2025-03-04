eb774198a1bb5a05f3c20632ab928eff26ac4a23
[MotilQR] <[pantenkov08@bk.ru]>
2025-03-04 17:31:47 +0300

    Fixed main button

---
+++ app/api/getLoc/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+// Временное хранилище местоположений
+const locations = new Map();
+
+/**
+ * GET-запрос для получения координат по коду
+ * @param {Request} req
+ */
+export async function GET(req) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const code = searchParams.get("code");
+
+    if (!code || !locations.has(code)) {
+      return NextResponse.json({ error: "Location not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(locations.get(code));
+  } catch (error) {
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,32 @@
 "use client";
-
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [startFlag, setStartFlag] = useState(false);
+  useEffect(() => {
+    if (startFlag) {
+
+    }
+  },[startFlag]) 
+  
+
   return (
     <div className="flex flex-col gap-5 mx-auto w-full max-w-2xl items-center justify-center h-screen">
-      <div className="flex flex-col bg-purple-800 p-6 w-[400px] rounded-lg shadow-lg items-center">
+      <div className="flex flex-col bg-purple-800 p-6 w-[400px] rounded-lg shadow-lg items-center px-4">
         <h1 className="font-mono text-2xl">Twitch IRL minimap</h1>
-        <p className="px-7 mt-5 text-center">This is web extension for OBS to display minimap on your IRL stream, press button to start!</p>
-        <button 
-          className="bg-white text-black py-1 px-7 rounded-2xl mt-5 font-mono text-xl"
-          
-        >
-          Start
-        </button>
+        {startFlag ? (
+          <p>Started</p>
+        ) : (
+          <div className="flex flex-col items-center">
+            <p className="px-7 mt-5 text-center">This is web extension for OBS to display minimap on your IRL stream, press button to start!</p>
+            <button 
+              className="bg-white text-black py-1 px-7 rounded-2xl mt-5 font-mono text-xl cursor-pointer hover:bg-gray-200 w-1/3"
+              onClick={() => setStartFlag(true)}
+            >
+              Start
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
