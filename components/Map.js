979ce0d4afb1097213b6d836b2b04f5c3df203b8
MotilQR <pantenkov08@bk.ru>
2025-03-08 15:02:13 +0300

    AGAINNN

--- components/Map.js
+++ components/Map.js
@@ -79,7 +79,10 @@ export default function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position} icon={user}></Marker>
+        <Marker 
+          position={position} icon={user}
+          style={{ transform: `rotate(${dir ? (-dir) : 0}deg)` }}
+        ></Marker>
       </MapContainer>
     ) : (
       <h1>Loading</h1>
