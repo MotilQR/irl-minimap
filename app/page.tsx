ac7f0b0f8e07f2d7d2667a551b4d630a246bb10b
MotilQR <pantenkov08@bk.ru>
2025-04-01 00:46:32 +0300

    Updated design

--- app/page.tsx
+++ app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-5 mx-auto w-full items-center mt-10">
       <div className="flex flex-col bg-purple-800 p-6 w-[400px] rounded-lg shadow-lg items-center px-4 shadow-purple-950 ">
-        <h1 className="font-mono text-2xl">Twitch IRL minimap</h1>
+        <h1 className="font-mono text-2xl font-semibold">Twitch IRL minimap</h1>
         {id ? (
           <div className="flex flex-col gap-2  items-center w-full">
             <div className="flex items-center mt-3 bg-purple-700 rounded-2xl shadow shadow-purple-950 w-full px-4">
@@ -93,7 +93,7 @@ export default function Home() {
             <input
               value={url}
               onChange={(event) => setUrl(event.target.value)}
-              className="bg-gray-300 rounded w-full text-black p-1"
+              className="bg-white text-black w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-900 hover:shadow shadow-purple-950"
               placeholder="Enter the stream URL"
             />
           </div>
@@ -101,7 +101,11 @@ export default function Home() {
           <div className="flex flex-col items-center">
             <p className="px-7 mt-5 text-center">This is web extension for OBS to display minimap on your IRL stream, press button to start!</p>
             <button 
-              className="bg-white text-black py-1 px-7 rounded-2xl mt-5 font-mono text-xl cursor-pointer hover:bg-gray-200 w-1/3"
+              className='px-7 py-1 mt-2 bg-white text-black  cursor-pointer select-none
+                active:translate-y-1  active:[box-shadow:0_0px_0_0_#e6e6e6,0_0px_0_0_#4e0c7d]
+                active:border-b-[0px]
+                transition-all duration-150 [box-shadow:0_4px_0_0_#e6e6e6,0_9px_0_0_#4e0c7d]
+                rounded-full  border-[0px] border-gray-400'
               onClick={() => setStartFlag(true)}
             >
               Start
